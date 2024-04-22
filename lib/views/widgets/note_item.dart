2b3b69b4c4dbe721408a964a7d945db2a93d3e6d
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-04-22 13:39:06 +0200

    display notes part two

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -23,21 +24,21 @@ class NoteItem extends StatelessWidget {
           bottom: 24,
         ),
         decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 24),
+              title: Text(
+                note.title,
+                style: const TextStyle(color: Colors.black, fontSize: 24),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16),
                 child: Text(
-                  'Build your career with tharwat samy',
+                  note.subTitlr,
                   style: TextStyle(
                       color: Colors.black.withOpacity(.5), fontSize: 18),
                 ),
@@ -54,7 +55,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 16, top: 16),
               child: Text(
-                'May21 , 2022',
+                note.date,
                 style: TextStyle(color: Colors.black.withOpacity(.4)),
               ),
             )
